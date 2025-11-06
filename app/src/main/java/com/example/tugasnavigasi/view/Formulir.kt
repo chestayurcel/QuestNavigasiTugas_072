95c56efae00b60af940ae76cd24ba991bd142b24
Yurcell <chestayurcel@gmail.com>
2025-11-06 22:40:00 +0700

    Menambahkan ElevatedCard untuk menampilkan data di Formulir.kt

--- app/src/main/java/com/example/tugasnavigasi/view/Formulir.kt
+++ app/src/main/java/com/example/tugasnavigasi/view/Formulir.kt
@@ -108,3 +108,17 @@ fun FormDataDiri(modifier: Modifier)
             thickness = dimensionResource(R.dimen.divider_tipis),
             color = Color.DarkGray
         )
+
+        ElevatedCard (
+            elevation = CardDefaults.cardElevation(defaultElevation = 10.dp),
+            colors = CardDefaults.cardColors(containerColor = Color.Black),
+            modifier = Modifier
+                .height(100.dp)
+                .width(300.dp)
+        ) {
+            Column (modifier = Modifier.padding(horizontal = 5.dp, vertical = 15.dp),){
+                Text(text = "Nama   : "+nama, color = Color.White)
+                Text(text = "Gender : "+jenis, color = Color.White)
+                Text(text = "Alamat : "+alamat, color = Color.White)
+            }
+        }
