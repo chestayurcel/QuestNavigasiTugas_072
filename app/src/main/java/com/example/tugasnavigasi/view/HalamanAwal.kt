f9385bbf5770c393d5ddd3477e012148fc8eecbb
Yurcell <chestayurcel@gmail.com>
2025-11-07 14:55:04 +0700

    Membuat UI untuk HalamanAwal.kt

--- app/src/main/java/com/example/tugasnavigasi/view/HalamanAwal.kt
+++ app/src/main/java/com/example/tugasnavigasi/view/HalamanAwal.kt
@@ -23,3 +23,65 @@ import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.example.tugasnavigasi.R
 import com.example.tugasnavigasi.Routes
+
+@Composable
+fun HalamanAwal(navController: NavController) {
+    val backgroundColor = Color(0xFFF3E5F5)
+    val textColor = Color(0xFF3F51B5)
+    val buttonColor = Color(0xFF2738B6)
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(backgroundColor)
+            .padding(32.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceAround
+    ) {
+        Spacer(modifier = Modifier.height(3.dp))
+        // Bagian Atas: Selamat Datang
+        Text(
+            text = "Selamat Datang",
+            fontSize = 40.sp,
+            fontWeight = FontWeight.Bold,
+            fontFamily = FontFamily.Monospace,
+            color = textColor,
+            style = TextStyle(lineHeight = 56.sp)
+        )
+
+        // Bagian Tengah: Logo dan Nama
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.logo_card_lst),
+                contentDescription = "Foto Saya Sendiri",
+                modifier = Modifier.size(280.dp)
+            )
+            Text(
+                text = "Chesta Yurcel Zebada\n20230140072",
+                fontSize = 20.sp,
+                fontFamily = FontFamily.Monospace,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center,
+                lineHeight = 24.sp
+            )
+        }
+
+        // Bagian Bawah: Tombol Submit
+        Button(
+            onClick = {
+                navController.navigate(Routes.LIST_DAFTAR)
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(50.dp),
+            shape = RoundedCornerShape(25.dp),
+            colors = ButtonDefaults.buttonColors(containerColor = buttonColor)
+        ) {
+            Text(text = "Submit", fontSize = 18.sp, color = Color.White)
+        }
+        Spacer(modifier = Modifier.height(30.dp))
+    }
+}
